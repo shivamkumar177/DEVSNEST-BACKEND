d808cd1c30cd63a036f787731c2aeea290411193
Shivam Kumar <shivamk1717@gmail.com>
2021-09-24 13:12:28 +0530

    added day 9 tha

---
+++ DAY 9/tha/routes/users.js
@@ -0,0 +1,12 @@
+var express = require('express');
+var router = express.Router();
+var path = require('path');
+/* GET users listing. */
+// router.get('/', function (req, res, next) {
+//   res.send('respond with a resource');
+// });
+
+router.get('/', function (req, res, next) {
+  res.send("hello");
+});
+module.exports = router;
